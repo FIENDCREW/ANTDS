0760cf951b5af271ae0631ae1f5811479b19e44a
FIENDCREW <odysseybmx911@gmail.com>
2022-10-01 23:55:52 +0300

    error

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import { Layout } from 'antd';
 import { useLocation, useNavigate } from 'react-router-dom';
-import style from './PageLayout.module.scss';
 import Header from 'shared/Header/Header';
 import Footer from 'shared/Footer/Footer';
+import style from './PageLayout.module.scss';
 
 const { Content } = Layout;
 
@@ -12,21 +12,12 @@ interface IPageLayout {
 }
 
 const PageLayout: FC<IPageLayout> = ({ children }) => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (pathname === '/') navigate('/home');
-  }, []);
-
-  const handleSiderToggle = () => setIsCollapsed((prev) => !prev);
-
-  // const collapsedSiderStn = isCollapsed ? (
-  //   <MenuUnfoldOutlined onClick={handleSiderToggle} />
-  // ) : (
-  //   <MenuFoldOutlined onClick={handleSiderToggle} />
-  // );
+  }, [navigate, pathname]);
 
   return (
     <Layout className={style.main_layout}>
